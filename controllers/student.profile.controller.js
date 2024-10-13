e2b10aaf38c1daa5a90a753ca59ceeceaec44494
pratham704 <pratham.siddannavar28@gmail.com>
2024-10-13 17:39:55 +0530

    Refactor student profile controller and routes
    This commit refactors the student profile controller and routes. It adds a new function, editStudentProfile, which allows users to update their profile information. The function checks for the existence of the user and updates the necessary fields based on the request body. Additionally, the commit updates the student routes to include the new edit profile endpoint.

--- controllers/student.profile.controller.js
+++ controllers/student.profile.controller.js
@@ -71,6 +71,38 @@ const getProfileChecker = async (req, res) => {
 
 
 
+const editStudentProfile = async (req, res) => {
+    try {
+        const { id } = jwt.getData(req);
+        const user = await User.findOne({ _id: id });
+        
+        if (!user)  return fail_response(res, 404, "User not found");
+
+        const updates = req.body;
+        if (updates.email) user.email = updates.email;
+        if (updates.firebaseUID)  user.firebaseUID = updates.firebaseUID;
+        if (typeof updates.paymentStatus === 'boolean') user.paymentStatus = updates.paymentStatus;
+        
+        if (updates.profileDetails) {
+            if (updates.profileDetails.name) user.profileDetails.name = updates.profileDetails.name;
+            if (updates.profileDetails.college) user.profileDetails.college = updates.profileDetails.college;
+            if (updates.profileDetails.phoneNumber) user.profileDetails.phoneNumber = updates.profileDetails.phoneNumber;
+            if (updates.profileDetails.imageURL) user.profileDetails.imageURL = updates.profileDetails.imageURL;
+            if (updates.profileDetails.collegeID)  user.profileDetails.collegeID = updates.profileDetails.collegeID;
+            if (updates.profileDetails.collegeIdUrl) user.profileDetails.collegeIdUrl = updates.profileDetails.collegeIdUrl;
+        }
+        await user.save();
+        success_response(res, 200, "Profile updated successfully");
+    } catch (error) {
+        console.error("Error updating profile:", error);
+        fail_response(res, 500, "Internal server error");
+    }
+};
+
+
+
+
+
 const getAllStudents = async (req, res) => {
     try {
         const students = await User.find();
@@ -83,5 +115,8 @@ const getAllStudents = async (req, res) => {
 
 
 
-export { getAllStudents, getStudentProfile, getProfileChecker }
+
+
+
+export { getAllStudents, getStudentProfile, getProfileChecker , editStudentProfile }
 
--- routes/student.routes.js
+++ routes/student.routes.js
@@ -1,21 +1,19 @@
 import express from "express";
 import * as authStudent from "../controllers/auth/auth.student.controller.js"
 import { authenticate } from "../middlewares/auth.middleware.js";
-import * as health  from "../controllers/healthCheck.controller.js";
+import * as health from "../controllers/healthCheck.controller.js";
 import * as profile from "../controllers/student.profile.controller.js"
 
 const router = express.Router();
 
-router.post("/register/" , authStudent.createStudent);
-router.post("/login/",  authStudent.loginStudent);
-router.get("/health_check/", health.healthCheck );
-router.get("/profile/" , authenticate ,  profile.getStudentProfile );
-router.get("/profile_checker/" , profile.getProfileChecker );
+router.post("/register/", authStudent.createStudent);
+router.post("/login/", authStudent.loginStudent);
+router.get("/health_check/", health.healthCheck);
+router.get("/profile/", authenticate, profile.getStudentProfile);
+router.get("/profile_checker/", authenticate, profile.getProfileChecker);
+router.post("/edit_profile/", authenticate, profile.editStudentProfile);
 
 
-
-router.get("/", authenticate, profile.getAllStudents);
-
-
+router.post("/", authenticate, profile.getAllStudents);
 
 export default router;
